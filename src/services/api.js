7a9a9e0d6137a4a0bc52d3b793d6417eae114f4b
RubsRafa <rubiarafaela2000@gmail.com>
2023-06-09 17:50:09 -0300

    build: install axios dayjs and react-icons

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import dayjs from "dayjs";
+
+const today = dayjs();
+const date = `${today.$y}-${today.$M}-${today.$D}`
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}?language=en&pageSize=10&from=${date}&sortBy=publishedAt&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
+
+export default baseURL;
